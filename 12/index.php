e903129aa5bf1c38dd24f0a9a428a2c988fae9d0
rasko-lazic <raskolazic@gmail.com>
2022-12-17 10:45:29 +0100

    final fix for task 12, added solution for second star in task 14

--- 12/index.php
+++ 12/index.php
@@ -29,6 +29,29 @@
 //
 //What is the fewest steps required to move from your current position to the location that should get the best signal?
 
+//--- Part Two ---
+//As you walk up the hill, you suspect that the Elves will want to turn this into a hiking trail. The beginning isn't very scenic, though; perhaps you can find a better starting point.
+//
+//To maximize exercise while hiking, the trail should start as low as possible: elevation a. The goal is still the square marked E. However, the trail should still be direct, taking the fewest steps to reach its goal. So, you'll need to find the shortest path from any square at elevation a to the square marked E.
+//
+//Again consider the example from above:
+//
+//Sabqponm
+//abcryxxl
+//accszExk
+//acctuvwj
+//abdefghi
+//Now, there are six choices for starting position (five marked a, plus the square marked S that counts as being at elevation a). If you start at the bottom-left square, you can reach the goal most quickly:
+//
+//...v<<<<
+//...vv<<^
+//...v>E^^
+//.>v>>>^^
+//>^>>>>>^
+//This path reaches the goal in only 29 steps, the fewest possible.
+//
+//What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?
+
 $rows = explode("\r\n", file_get_contents("./input.txt"));
 
 $grid = [];
@@ -52,43 +75,59 @@ foreach ($rows as $index => $row) {
 }
 // Put grid in a constant to make it available for helper functions
 define("GRID", $grid);
+define("START", $start);
+define("END", $end);
 
-$potentialPaths = [[$start]];
-$shortestPath = INF;
-while (count($potentialPaths) > 0) {
-    $potentialPaths = array_filter($potentialPaths, fn ($path) => count($path) < $shortestPath);
-    $currentPath = array_pop($potentialPaths);
+$shortestPathLength = findShortestPath($start, $end);
+echo "Shortest path up the hill is $shortestPathLength steps long\n\n";
 
-    while (true) {
-        $lastStep = end($currentPath);
-        if ($lastStep === $end) {
-            if ($shortestPath > count($currentPath)) {
-                $shortestPath = count($currentPath);
-                var_dump($shortestPath);
-            }
-            break;
-        }
-
-        $possibleSteps = getNextSteps($lastStep, $currentPath);
-        shuffle($possibleSteps);
-        $nextStep = array_pop($possibleSteps);
-        foreach ($possibleSteps as $possibleStep) {
-            $potentialPaths[] = [...$currentPath, $possibleStep];
-        }
-
-        if ($nextStep === null || count($currentPath) > $shortestPath) {
-            break;
-        } else {
-            $currentPath[] = $nextStep;
-            renderScreen($currentPath);
-            usleep(500000);
-        }
-
-//        var_dump($currentPath, $potentialPaths);
-    }
+//--- Part Two ---
+$startingSpots = array_map(fn ($item) => [$item, 0], range(0, count($grid) - 1));
+$shortestPathLength = INF;
+foreach ($startingSpots as $startingSpot) {
+    $pathLength = findShortestPath($startingSpot, $end);
+    $shortestPathLength = $shortestPathLength > $pathLength ? $pathLength : $shortestPathLength;
 }
-var_dump($shortestPath);
-exit();
+echo "Best path for scenic route is $shortestPathLength steps long\n\n";
+
+//--- Incorrect approach ---
+
+// This approach leads to too much testing of already tested steps, it is too slow
+
+//$potentialPaths = [[$start]];
+//$shortestPath = INF;
+//while (count($potentialPaths) > 0) {
+//    $potentialPaths = array_filter($potentialPaths, fn ($path) => count($path) < $shortestPath);
+//    $currentPath = array_pop($potentialPaths);
+//
+//    while (true) {
+//        $lastStep = end($currentPath);
+//        if ($lastStep === $end) {
+//            if ($shortestPath > count($currentPath)) {
+//                $shortestPath = count($currentPath);
+//                var_dump($shortestPath);
+//            }
+//            break;
+//        }
+//
+//        $possibleSteps = getNextSteps($lastStep, $currentPath);
+//        shuffle($possibleSteps);
+//        $nextStep = array_pop($possibleSteps);
+//        foreach ($possibleSteps as $possibleStep) {
+//            $potentialPaths[] = [...$currentPath, $possibleStep];
+//        }
+//
+//        if ($nextStep === null || count($currentPath) > $shortestPath) {
+//            break;
+//        } else {
+//            $currentPath[] = $nextStep;
+//            renderPath($currentPath);
+//            usleep(500000);
+//        }
+//    }
+//}
+//var_dump($shortestPath);
+//exit();
 
 // This recursion doesn't work since it keeps creating new instances
 // Every branching means the program steps into new function, which doesn't get resolved until all later branches get resolved
@@ -130,7 +169,7 @@ exit();
 //    }
 //}
 
-function getNextSteps($currentLocation, $partialPath): array {
+function getNextSteps($currentLocation, $visited, $stepQueue): array {
     $allNextSteps = [
         [$currentLocation[0] - 1, $currentLocation[1]],
         [$currentLocation[0], $currentLocation[1] + 1],
@@ -141,16 +180,66 @@ function getNextSteps($currentLocation, $partialPath): array {
         $allNextSteps,
         fn ($step) => isset(GRID[$step[0]][$step[1]]) &&
         GRID[$step[0]][$step[1]] - GRID[$currentLocation[0]][$currentLocation[1]] < 2 &&
-        !in_array($step, $partialPath)
+        !isset($visited[implode('-', $step)]) &&
+        !isset($stepQueue[implode('-', $step)])
     ));
 }
 
-function renderScreen($path): void
+//function renderPath($path): void
+//{
+//    foreach (GRID as $rowKey => $row) {
+//        $mappedRows[] = implode('.', array_map(fn ($item, $columnKey) => in_array([$rowKey, $columnKey], $path) ? "-" : 'o', $row, array_keys($row)));
+//    }
+//    $implodedRows = implode("\n", $mappedRows);
+//    popen('cls', 'w');
+//    echo "$implodedRows";
+//}
+
+function findShortestPath($start, $end): int
+{
+    $visited = [];
+    $traversalMap = [];
+    $stepQueue = [implode('-', $start) => $start];
+    while (count($stepQueue) > 0) {
+        shuffle($stepQueue);
+        $currentStep = array_shift($stepQueue);
+        $visited[implode('-', $currentStep)] = 1;
+
+        if ($currentStep === $end) {
+            break;
+        }
+
+        $possibleSteps = getNextSteps($currentStep, $visited, $stepQueue);
+        shuffle($possibleSteps);
+        foreach ($possibleSteps as $possibleStep) {
+            $traversalMap[implode('-', $possibleStep)] = $currentStep;
+            $stepQueue[implode('-', $possibleStep)] = $possibleStep;
+        }
+
+        renderGrid($visited);
+        usleep(200);
+    }
+    return count(createPath($end, $traversalMap)) - 1;
+}
+
+function createPath($destination, $map): array
+{
+    $path = [$destination];
+    $previousStep = $map[implode('-', $destination)];
+    while (true) {
+        array_push($path, $previousStep);
+        if (!isset($map[implode('-', $previousStep)])) break;
+        $previousStep = $map[implode('-', $previousStep)];
+    }
+    return array_reverse($path);
+}
+
+function renderGrid($visited): void
 {
     foreach (GRID as $rowKey => $row) {
-        $mappedRows[] = implode('.', array_map(fn ($item, $columnKey) => in_array([$rowKey, $columnKey], $path) ? "-" : 'o', $row, array_keys($row)));
+        $mappedRows[] = implode('.', array_map(fn ($item, $columnKey) => isset($visited["$rowKey-$columnKey"]) ? "-" : 'o', $row, array_keys($row)));
     }
     $implodedRows = implode("\n", $mappedRows);
     popen('cls', 'w');
-    echo "$implodedRows";
+    echo $implodedRows;
 }
